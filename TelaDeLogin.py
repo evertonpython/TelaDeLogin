0610e4d90af8c21419e922b9221d6281a07cfccd
Everton <46085461+evertonpython@users.noreply.github.com>
2019-03-26 13:34:38 -0300

    Add files via upload
    Tela de login básica com Python2 e seu framework para recursos gráficos Tkinter.

---
+++ TelaDeLogin.py
@@ -0,0 +1,35 @@
+from tkinter import *
+
+def ver():
+	if ed1.get() == "everton" and ed2.get() == "everton":
+		print("LOGADO COM SUCESSO!!!")
+		
+	else:
+		print("LOGIN OU SENHA INVALIDOS!!!")
+		
+	
+	
+janela = Tk()
+ 
+janela.title("LOGIN") 
+ 
+
+lb1 = Label(janela, text="Senha: ")
+lb2 = Label(janela, text="Login: ")
+
+ed1 = Entry(janela) 
+ed2 = Entry(janela, show="*")
+
+bt1 = Button(janela, text="Confirmar", command= ver)
+
+lb2.grid(row=0, column=0)
+lb1.grid(row=1, column=0)
+ed1.grid(row=0, column=1)
+ed2.grid(row=1, column=1)
+bt1.grid(row=2, column=1)
+
+
+janela.geometry("200x100+100+100")
+
+ 
+janela.mainloop()
